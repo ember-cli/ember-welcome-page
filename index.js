a6eaacc8b4affc3013968e4952cd8f842800ee50
David Baker <dbaker@acorncomputersolutions.com>
2016-02-24 22:28:03 -0700

    Shifted application.hbs and styles.css to be conditionally added

--- index.js
+++ index.js
@@ -1,6 +1,43 @@
 /* jshint node: true */
 'use strict';
 
+var fs    = require('fs');
+var path  = require('path');
+
 module.exports = {
-  name: 'ember-welcome-page'
+  name: 'ember-welcome-page',
+
+  included: function(app) {
+    this._super.included(app);
+
+    this.app = app;
+
+    this.shouldIncludeFiles = this._shouldIncludeFiles();
+  },
+
+  contentFor: function(type, config) {
+    if (type === 'body' && this.shouldIncludeFiles) {
+      return fs.readFileSync(path.join(__dirname, 'vendor/welcome.html'), 'utf8');
+    }
+
+    if (type === 'head-footer' && this.shouldIncludeFiles) {
+      return '<style>' + fs.readFileSync(path.join(__dirname, 'vendor/styles.css'), 'utf8') + '</style>';
+    }
+  },
+
+  _shouldIncludeFiles: function() {
+    // @TODO: improve logic here further to check the parent app (this.app) with various checks
+    // see https://github.com/samselikoff/ember-cli-mirage/blob/master/index.js for some initial
+    // material on how to tackle this type of checking
+
+    /* requested tests
+      - Has an application.hbs been created?
+      - Has an index.hbs been created?
+      - Have any routes been created?
+    */
+
+    // @TODO: figure out how to write tests for this?  painful ...
+
+    return true;
+  }
 };
