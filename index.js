2fded2a1be94ba78015ee4fee3c4622a068da6b1
David Baker <dbaker@acorncomputersolutions.com>
2016-03-11 10:45:05 -0700

    Moved things into a middleware route per rwjblue

--- index.js
+++ index.js
@@ -3,8 +3,6 @@
 
 var fs    = require('fs');
 var path  = require('path');
-var mergeTrees = require('broccoli-merge-trees');
-var Funnel = require('broccoli-funnel');
 
 module.exports = {
   name: 'ember-welcome-page',
@@ -14,32 +12,28 @@ module.exports = {
 
     this.app = app;
 
-    this.welcomeDirectory = path.join(this.project.addonPackages['ember-welcome-page'].path, '/welcome');
-
     this.shouldIncludeFiles = this._shouldIncludeFiles();
-
-    if (this.shouldIncludeFiles) {
-      app.import('vendor/app.css');
-    }
   },
 
-  // handle merging in our application.hbs file
-  // because we are doing things here (without an overwrite), this will automatically
-  // turn off if an application.hbs file is created in the parent app
-  treeForTemplates: function(tree) {
-    if (!this._shouldIncludeFiles) {
-      return tree;
-    }
+  serverMiddleware: function(config) {
+    var app = config.app;
+    var options = config.options;
 
-    var welcomeTemplates = new Funnel(this.welcomeDirectory, {
-      srcDir: 'templates'
+    console.log(`If you're just getting started with ember, please visit http://localhost:${options.port}${options.baseURL}ember-getting-started to get going`);
+
+    app.use(options.baseURL + 'ember-getting-started', function(request, response, next) {
+
+      var stream = fs.createReadStream(path.join(__dirname, 'vendor/welcome.html'));
+      stream.on('error', function(e) {
+        response.status(404).send('not found');
+      });
+
+      response.set('Content-Type', 'text/html');
+      stream.pipe(response);
     });
 
-    if (tree) {
-      return mergeTrees([tree, welcomeTemplates]);
-    } else {
-      return welcomeTemplates;
-    }
+    // @TODO: we'll add other middleware down here to hijack / (by leaving out next)
+    // once we're sure our tests below are working
   },
 
   _shouldIncludeFiles: function() {
