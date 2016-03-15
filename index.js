e9098ca0b73a590baaaee5b360c43dfc9e8c4cfc
David Baker <dbaker@acorncomputersolutions.com>
2016-03-14 21:38:49 -0600

    Initial setup of tests (but doesn't matter as override doesn't happen yet)

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 /* jshint node: true */
 'use strict';
 
+var existsSync = require('exists-sync');
+var fs = require('fs');
+var path = require('path');
+var walkSync = require('walk-sync');
+
 module.exports = {
   name: 'ember-welcome-page',
 
@@ -9,7 +14,7 @@ module.exports = {
 
     this.app = app;
 
-    this.shouldIncludeFiles = this._shouldIncludeFiles();
+    this.shouldOverrideIndex = this.shouldOverride(this.app.registry, this.app.project.root);
   },
 
   serverMiddleware: function(config) {
@@ -33,23 +38,62 @@ module.exports = {
       });
     });
 
-    // @TODO: we'll add other middleware down here to hijack / (by leaving out next)
+    // we'll add other middleware down here to hijack / (by leaving out next)
     // once we're sure our tests below are working
+    //if (this.shouldOverrideIndex) {
+    if (true) {
+      app.get('/', function (req, res, next) {
+        res.sendFile(__dirname + '/vendor/welcome.html', options, function (err) {
+          if (err) {
+            res.status(err.status).end();
+          }
+        });
+      });
+    }
   },
 
-  _shouldIncludeFiles: function() {
-    // @TODO: improve logic here further to check the parent app (this.app) with various checks
-    // see https://github.com/samselikoff/ember-cli-mirage/blob/master/index.js for some initial
-    // material on how to tackle this type of checking
+  /* requested tests (need to be watching in real-time preferably ...)
+   - Has an application.hbs been created?
+   - Has an index.hbs been created?
+   - Have any routes been created?
+   */
+  shouldOverride: function(registry, folderPath) {
+    var templateExtensions = registry.extensionsForType('template');
+    var jsExtensions = registry.extensionsForType('js');
+    var appFiles = walkSync(path.join(folderPath, 'app'));
 
-    /* requested tests (need to be watching in real-time preferably ...)
-      - Has an application.hbs been created?
-      - Has an index.hbs been created?
-      - Have any routes been created?
-    */
+    var jsFiles = appFiles
+      .filter(function(fileName) {
+        var extension = path.extname(fileName).slice(1); // remove leading `.`
 
-    // @TODO: figure out how to write tests for this?  painful ...
+        return jsExtensions.indexOf(extension) > -1;
+      });
 
-    return true;
+    var templateFiles = appFiles
+      .filter(function(fileName) {
+        var extension = path.extname(fileName).slice(1); // remove leading `.`
+
+        return templateExtensions.indexOf(extension) > -1;
+      });
+
+    var routeFiles = jsFiles
+      .filter(function(fileName) {
+        var baseName = path.basename(fileName);
+
+        return baseName === 'route' || fileName.match(/^routes/);
+      });
+
+    var routerPath = path.join(folderPath, 'app', 'router.js');
+    var hasRouterMapContents = false;
+
+    if (existsSync(routerPath)) {
+      var router = fs.readFileSync(routerPath, { encoding: 'utf8' });
+      hasRouterMapContents = router.match(/this\.route|this\.resource/);
+    }
+
+    var hasTemplates = templateFiles.length > 0;
+    var hasRoutes = routeFiles.length > 0;
+
+    return !hasTemplates && !hasRoutes && !hasRouterMapContents;
   }
 };
