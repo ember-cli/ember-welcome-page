635a156be7a902e238a1a079612558dc43e2e177
Ricardo Mendes <rokusu@gmail.com>
2016-10-25 15:06:23 +0100

    Changes architecture into addon component that is to be included in the default application.hbs for a project. (#58)
    - Better ergonomics by teaching users about application.hbs and components
    - Conditional assets so no extra weight is added to production builds

---
+++ tests/integration/components/welcome-page-test.js
@@ -0,0 +1,21 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+import Ember from 'ember';
+
+moduleForComponent('welcome-page', 'Integration | Component | welcome page', {
+  integration: true
+});
+
+test('it renders', function(assert) {
+  // Set any properties with this.set('myProperty', 'value');
+  // Handle any actions with this.on('myAction', function(val) { ... });
+
+  this.render(hbs`{{welcome-page}}`);
+
+  let [ emberMajor, emberMinor ] = Ember.VERSION.split('.');
+  let [ welcomeMajor, welcomeMinor, welcomePatch ] = this.$('[data-ember-version]').data('ember-version').split('.');
+
+  assert.equal(emberMajor, welcomeMajor, "Major segment of version should match.");
+  assert.equal(emberMinor, welcomeMinor, "Minor segment of version should match.");
+  assert.equal("0", welcomePatch, "Patch segment of version should be 0.");
+});
