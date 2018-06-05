a764488ec5f4b4548cb7bf05ceedd926773b4d85
Devin Weaver <suki@tritarget.org>
2018-06-05 17:43:32 -0400

    Add a dependencies table to welcome-page component
    This Adds a table which is toggled by a button.

--- addon/components/welcome-page.js
+++ addon/components/welcome-page.js
@@ -1,8 +1,11 @@
 import Ember from 'ember';
 import layout from '../templates/components/welcome-page';
+import AppDependencyDescriptors from '../-app-dependency-descriptors';
 
 export default Ember.Component.extend({
   layout,
+
+  dependencies: AppDependencyDescriptors,
 
   emberVersion: Ember.computed(function() {
     let [ major, minor ] = Ember.VERSION.split(".");
