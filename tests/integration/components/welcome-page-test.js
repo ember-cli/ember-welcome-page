62e5c248dc8e37047a3126a3760b54af2bd049a4
Robert Wagner <rwwagner90@gmail.com>
2020-06-11 14:26:34 -0400

    Update tests/integration/components/welcome-page-test.js
    Co-authored-by: Isaac Lee <16869656+ijlee2@users.noreply.github.com>

--- tests/integration/components/welcome-page-test.js
+++ tests/integration/components/welcome-page-test.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
-import hbs from 'htmlbars-inline-precompile';
+import { hbs } from 'ember-cli-htmlbars';
 
 module('Integration | Component | welcome page', function(hooks) {
   setupRenderingTest(hooks);
