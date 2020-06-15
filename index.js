83af7d0a6d42e65bef8a8577ed0f0b3ea51200d9
Robert Wagner <rwwagner90@gmail.com>
2020-06-15 15:59:45 -0400

    Remove all _isDisabled checks

--- index.js
+++ index.js
@@ -6,10 +6,6 @@ module.exports = {
   included(app) {
     this._super.included.apply(this, arguments);
 
-    this._welcomeConfig = app.options['ember-welcome-page'] || {};
-
-    if (this._isDisabled()) { return; }
-
     app.import('vendor/welcome-page.css');
   },
 
@@ -20,33 +16,5 @@ module.exports = {
         isModuleUnification: project.isModuleUnification && project.isModuleUnification()
       };
     }
-  },
-
-  treeForPublic() {
-    if (this._isDisabled()) { return false; }
-
-    var tree = this._super.treeForPublic.apply(this, arguments);
-
-    return tree;
-  },
-
-  treeForAddon() {
-    if (this._isDisabled()) { return false; }
-
-    var tree = this._super.treeForAddon.apply(this, arguments);
-
-    return tree;
-  },
-
-  treeForApp() {
-    if (this._isDisabled()) { return false; }
-
-    var tree = this._super.treeForApp.apply(this, arguments);
-
-    return tree;
-  },
-
-  _isDisabled() {
-    return this._welcomeConfig.enabled === false;
   }
 };
