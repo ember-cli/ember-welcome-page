6424beb12f1c8ed4963c9daa86077b2180cee3aa
Katie Gengler <katie@kmg.io>
2021-09-09 17:32:32 -0400

    Add configuration for github actions CI
    - Modify tests to use Ember's VERSION as-is, as it can no longer be
      modified
    - Update implementation to also link to /current docs for any
      not-explicitly-stable VERSION value

--- tests/integration/components/welcome-page-test.js
+++ tests/integration/components/welcome-page-test.js
@@ -1,58 +1,29 @@
-/* eslint-disable ember/new-module-imports */
-import Ember from 'ember';
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
+import { VERSION } from '@ember/version';
 import { hbs } from 'ember-cli-htmlbars';
+import semver from 'semver';
 
 module('Integration | Component | welcome page', function(hooks) {
   setupRenderingTest(hooks);
 
-  test('it links to version for release version', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.on('myAction', function(val) { ... });
-    Ember.VERSION = '3.1.5';
+  test('it links to correct docs for the version of ember', async function(assert) {
 
     await render(hbs`<WelcomePage/>`);
 
-    let [ emberMajor, emberMinor ] = Ember.VERSION.split('.');
-    let [ welcomeMajor, welcomeMinor, welcomePatch ] = this.element.querySelector('[data-ember-version]').dataset.emberVersion.split('.');
+    if (semver.valid(VERSION) && !semver.prerelease(VERSION)) {
+      let [emberMajor, emberMinor] = VERSION.split('.');
+      let [welcomeMajor, welcomeMinor, welcomePatch] = this.element.querySelector(
+        '[data-ember-version]').dataset.emberVersion.split('.');
 
-    assert.equal(emberMajor, welcomeMajor, "Major segment of version should match.");
-    assert.equal(emberMinor, welcomeMinor, "Minor segment of version should match.");
-    assert.equal("0", welcomePatch, "Patch segment of version should be 0.");
-  });
+      assert.equal(emberMajor, welcomeMajor, "Major segment of version should match.");
+      assert.equal(emberMinor, welcomeMinor, "Minor segment of version should match.");
+      assert.equal("0", welcomePatch, "Patch segment of version should be 0.");
+    } else {
+      let versionText = this.element.querySelector('[data-ember-version]').dataset.emberVersion;
 
-  test('it links to "/current" for alpha versions', async function(assert) {
-    // Set the version property
-    Ember.VERSION = '2.15.0-alpha.1';
-
-    await render(hbs`<WelcomePage/>`);
-
-    let versionText = this.element.querySelector('[data-ember-version]').dataset.emberVersion;
-
-    assert.equal(versionText, 'current', "Version text should be set to 'current' when an alpha version is used.");
-  });
-
-  test('it links to "/current" for beta versions', async function(assert) {
-    // Set the version property
-    Ember.VERSION = '2.15.0-beta.1';
-
-    await render(hbs`<WelcomePage/>`);
-
-    let versionText = this.element.querySelector('[data-ember-version]').dataset.emberVersion;
-
-    assert.equal(versionText, 'current', "Version text should be set to 'current' when a beta version is used.");
-  });
-
-  test('it links to "/current" for master', async function(assert) {
-    // Set the version property
-    Ember.VERSION = 'master';
-
-    await render(hbs`<WelcomePage/>`);
-
-    let versionText = this.element.querySelector('[data-ember-version]').dataset.emberVersion;
-
-    assert.equal(versionText, 'current', "Version text should be set to 'current' when master is used.");
+      assert.equal(versionText, 'current', "Version text should be set to 'current' when a non-stable release is used.");
+    }
   });
 });
