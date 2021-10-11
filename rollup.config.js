a511b069144b0d1f2da6034f81bd1d5d89ce7d19
Edward Faulkner <ef@alum.mit.edu>
2021-10-11 15:52:48 -0400

    more rollup work

--- rollup.config.js
+++ rollup.config.js
@@ -1,31 +1,18 @@
 import babel from '@rollup/plugin-babel';
-import {
-  hbs,
-  publicEntrypoints,
-  clean,
-  appReexports,
-} from '@embroider/addon-dev/rollup';
+import { Addon } from '@embroider/addon-dev/rollup';
+
+const addon = new Addon();
 
 export default {
-  external: /\.css$/,
-  output: {
-    dir: 'dist',
-    format: 'es',
-    entryFileNames: '[name]',
-  },
+  output: addon.output(),
   plugins: [
-    publicEntrypoints({
-      srcDir: 'src',
-      include: ['components/**/*.js', 'index.js'],
-    }),
-    appReexports({
-      srcDir: 'src',
-      include: ['components/welcome-page.js'],
-    }),
+    addon.publicEntrypoints(['components/**/*.js', 'index.js']),
+    addon.appReexports(['components/welcome-page.js']),
     babel({
       plugins: ['@embroider/addon-dev/template-colocation-plugin'],
     }),
-    hbs(),
-    clean({ targets: 'dist/*' }),
+    addon.hbs(),
+    addon.keepAssets(['**/*.css']),
+    addon.clean(),
   ],
 };
