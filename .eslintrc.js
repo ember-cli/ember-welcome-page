e3b6686dbffed73a62519d37190c15675c42dc87
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2022-12-17 11:10:51 +0100

    Updated eslint to v8.30.0 (#390)
    * chore: Updated eslint to v8.30.0
    
    * chore: Ran yarn install
    
    * chore: Replaced eslint-plugin-node with eslint-plugin-n
    
    * chore: Ran yarn install
    
    Co-authored-by: ijlee2 <ijlee2@users.noreply.github.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,12 +2,15 @@
 
 module.exports = {
   root: true,
-  parser: 'babel-eslint',
+  parser: '@babel/eslint-parser',
   parserOptions: {
     ecmaVersion: 2018,
     sourceType: 'module',
-    ecmaFeatures: {
-      legacyDecorators: true,
+    requireConfigFile: false,
+    babelOptions: {
+      plugins: [
+        ['@babel/plugin-proposal-decorators', { decoratorsBeforeExport: true }],
+      ],
     },
   },
   plugins: ['ember'],
@@ -25,9 +28,9 @@ module.exports = {
     {
       files: [
         '.eslintrc.js',
+        '.prettierrc.js',
         '.template-lintrc.js',
         'addon-main.js',
-        '.prettierrc.js',
       ],
       parserOptions: {
         sourceType: 'script',
@@ -36,8 +39,7 @@ module.exports = {
         browser: false,
         node: true,
       },
-      plugins: ['node'],
-      extends: ['plugin:node/recommended'],
+      extends: ['plugin:n/recommended'],
     },
   ],
 };
