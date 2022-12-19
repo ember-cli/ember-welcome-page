cb862e47c606469c3bc9f8c598f33ec24b0cbad7
ijlee2 <ijlee2@users.noreply.github.com>
2022-12-19 20:10:47 +0100

    chore: Introduced TypeScript to ember-welcome-page

--- ember-welcome-page/src/components/welcome-page.ts
+++ ember-welcome-page/src/components/welcome-page.ts
@@ -1,42 +1,43 @@
 import { getOwner } from '@ember/application';
+/* @ts-expect-error: Cannot find module '@ember/version' or its corresponding type declarations. */
 import { VERSION } from '@ember/version';
 import Component from '@glimmer/component';
 import './welcome-page.css';
 
-function isLatestVersion() {
+function isLatestVersion(): boolean {
   const stableRegex = /^\d+\.\d+\.\d+$/;
 
-  return !stableRegex.test(VERSION);
+  return !stableRegex.test(VERSION as string);
 }
 
 export default class WelcomePageComponent extends Component {
-  get rootURL() {
+  get rootURL(): string {
     const config = getOwner(this).factoryFor('config:environment');
 
     if (config) {
-      return config.class.rootURL;
+      return (config.class as unknown as { rootURL: string }).rootURL;
     }
 
     return '/';
   }
 
-  get emberVersion() {
+  get emberVersion(): string {
     if (isLatestVersion()) {
       return 'current';
     }
 
-    const [majorVersion, minorVersion] = VERSION.split('.');
+    const [majorVersion, minorVersion] = (VERSION as string).split('.');
     const emberVersion = `${majorVersion}.${minorVersion}.0`;
 
     return emberVersion;
   }
 
-  get urlForEmberGuides() {
+  get urlForEmberGuides(): string {
     if (isLatestVersion()) {
       return `https://guides.emberjs.com/current`;
     }
 
-    const [majorVersion, minorVersion] = VERSION.split('.');
+    const [majorVersion, minorVersion] = (VERSION as string).split('.');
     const emberVersion = `${majorVersion}.${minorVersion}.0`;
 
     return `https://guides.emberjs.com/v${emberVersion}`;
