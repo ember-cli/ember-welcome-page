ab1740325c050d0ee9de90f848c9da1a1449e781
ijlee2 <ijlee2@users.noreply.github.com>
2023-05-25 10:27:40 +0200

    chore: Introduced Glint to test-app to check that ember-welcome-page bundles template-registry.ts correctly

--- ember-welcome-page/unpublished-development-types/index.d.ts
+++ ember-welcome-page/unpublished-development-types/index.d.ts
@@ -3,7 +3,7 @@
 
 import '@glint/environment-ember-loose';
 
-import EmberWelcomePageRegistry from '../src/template-registry';
+import type EmberWelcomePageRegistry from '../src/template-registry';
 
 declare module '@glint/environment-ember-loose/registry' {
   // Remove this once entries have been added! 👇
